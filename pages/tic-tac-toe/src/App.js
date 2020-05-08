ab313341cd49fa08fd2b3c6978abc6bdc3e9c6c8
tamlt2704 <tamlt2704@gmail.com>
2020-05-08 07:02:57 +0000

    add alpha beta minimax

--- pages/tic-tac-toe/src/App.js
+++ pages/tic-tac-toe/src/App.js
@@ -48,41 +48,220 @@ class App extends React.Component {
         super(props);
         this.state = {
             history: [
-                {squares: Array(9).fill(null)}
+                //{squares: Array(9).fill(null)}
+                {squares: [null, null, null, null, null, null, null, null, null]}
             ],
             stepNumber: 0,
-            xIsNext: true
+            xIsNext: true 
         }
     }
    
+    // X: Max
+    // O: Min (computer)
+    /*
+    alphaBetaMinimax(squares, alpha, beta, maximizingPlayer) {
+        var player = maximizingPlayer ? 'X' : 'O';
+        var winner = calculateWinner(squares);
+        var bestPosition = null;
+        console.log(squares
+            .map(x => x!=null? x : '*')
+            .map((x, i) => (i+1) % 3 ? x : x + '\n').join()
+            .replace(/,/g, '')
+        );
+        console.log(`current player ${player}`);
+        //if (winner === player) {
+        if (winner === player) {
+            console.log(`(${alpha}: ${beta}) ${maximizingPlayer ? "max" : "min"} return 1`);
+            return [-1, bestPosition];
+        } else if (winner != null ) {
+            console.log(`(${alpha}: ${beta}) ${maximizingPlayer ? "max" : "min"} return -1`);
+            return [1, bestPosition];
+        } else {
+            // board is full
+            if (squares.every(x => x)) {
+                console.log('tie');
+                return [0, bestPosition];
+            } 
+        }
+        console.log('continue');
+
+        if (maximizingPlayer) {
+            var value = -2;
+            for (let i = 0; i < 9; i++) {
+                if (!squares[i]) {
+                    squares[i] = player; //X
+                    var [a, pos] = this.alphaBetaMinimax(
+                        squares.slice(), alpha, beta, false);
+                    if (a > value) {
+                        value = a;
+                        bestPosition = i;
+                    }
+                    squares[i] = null;
+
+                    if (value >= beta) { 
+                        i = 10; 
+                    } //break for loop
+
+                    if (value > alpha) {
+                        alpha = value
+                    }
+                }
+            }
+            return [value, bestPosition];
+        } else {
+            var value = 2;
+            for (let i = 0; i < 9; i++) {
+                if (!squares[i]) {
+                    squares[i] = player; // O
+                    console.log('min set value for: ' + i + ':' + player); 
+                    var [b, pos] = this.alphaBetaMinimax(
+                        squares.slice(), alpha, beta, true);
+                    if (b < value) {
+                        value = b;
+                        bestPosition = i;
+                    }
+                    squares[i] = null;
+                    if (alpha >= value) { 
+                        i = 10; 
+                    } //break for loop
+
+                    if (value < beta) {
+                        beta = value;
+                    }
+                }
+            }
+            return [value, bestPosition];
+        }
+    }
+    */
+    minF(squares, depth, trace) {
+        var minv = 2
+        var bestPosition = null;
+        var winner = calculateWinner(squares);
+
+        console.log('minF'+ depth);
+        if (winner === 'X') {
+            console.log(squares
+                .map(x => x!=null? x : '*')
+                .map((x, i) => (i+1) % 3 ? x : x + '\n').join()
+                .replace(/,/g, '')
+            );
+            console.log('return ' + [-1, bestPosition]);
+            return [-1, bestPosition];
+        } else if (winner === 'O') {
+            console.log(squares
+                .map(x => x!=null? x : '*')
+                .map((x, i) => (i+1) % 3 ? x : x + '\n').join()
+                .replace(/,/g, '')
+            );
+            console.log('return ' + [1, bestPosition]);
+            return [1, bestPosition];
+        } else if (squares.every(x => x)) {
+            console.log('tie')
+            console.log(squares
+                .map(x => x!=null? x : '*')
+                .map((x, i) => (i+1) % 3 ? x : x + '\n').join()
+                .replace(/,/g, '')
+            );
+            console.log('return ' + [0, bestPosition]);
+            return [0, bestPosition];
+        }
+
+        for (let i = 0; i < 9; i++) {
+            if (!squares[i]) {
+                squares[i] = 'X';
+                var [m, p] = this.maxF(squares.slice(), depth+1);
+                if (m < minv) {
+                    minv = m;
+                    bestPosition = i;
+                }
+                squares[i] = null;
+            }
+        }
+        console.log('return' + [minv, bestPosition]);
+        return [minv, bestPosition];
+    }
+
+    maxF(squares, depth) {
+        var maxv = -2;
+        var bestPosition = null;
+        var winner = calculateWinner(squares);
+
+        console.log('maxF'+ depth);
+        if (winner === 'X') {
+            console.log(squares
+                .map(x => x!=null? x : '*')
+                .map((x, i) => (i+1) % 3 ? x : x + '\n').join()
+                .replace(/,/g, '')
+            );
+            console.log('return ' + [-1, bestPosition]);
+            return [-1, bestPosition];
+        } else if (winner === 'O') {
+            console.log(squares
+                .map(x => x!=null? x : '*')
+                .map((x, i) => (i+1) % 3 ? x : x + '\n').join()
+                .replace(/,/g, '')
+            );
+            console.log('return ' + [1, bestPosition]);
+            return [1, bestPosition];
+        } else if (squares.every(x => x)) {
+            console.log('tie')
+            console.log(squares
+                .map(x => x!=null? x : '*')
+                .map((x, i) => (i+1) % 3 ? x : x + '\n').join()
+                .replace(/,/g, '')
+            );
+            console.log('return ' + [0, bestPosition]);
+            return [0, bestPosition];
+        }
+
+        for (let i = 0; i < 9; i++) {
+            if (!squares[i]) {
+                squares[i] = 'O';
+                var [m, p] = this.minF(squares.slice(), depth+1);
+                if (m > maxv) {
+                    maxv = m;
+                    bestPosition = i;
+                }
+                squares[i] = null;
+            }
+        }
+        console.log('return' + [maxv, bestPosition]);
+        return [maxv, bestPosition];
+    }
+
+    findNextPosition(squares) {
+        /*var [value, nextPosition] = this.alphaBetaMinimax(
+            squares.slice(), -2, 2, false);
+            */
+        var trace = {};
+        var [value, nextPosition] = this.maxF(squares.slice(), 0, trace);
+        console.log(value, nextPosition);
+        return nextPosition;
+    }
+
     computerThinkAndPlay() {
-        console.log('computer think and play');
         const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
 
-        // get first empty square 
-        for (let i = 0; i < squares.length; i++) {
-            if (!squares[i]) {
-                var nextPosition = i;
-                console.log(nextPosition);
-                squares[nextPosition] = this.state.xIsNext ? "X" : "O";
+        var foundId = this.findNextPosition(squares);
+        if (foundId) {
+            squares[foundId] = this.state.xIsNext ? 'X' : 'O';
+        }
 
-
-                this.setState({
-                    history: history.concat([
-                        {
-                            squares: squares
-                        }
-                    ]),
-                    stepNumber: history.length,
-                    xIsNext: !this.state.xIsNext
-                }, () => { 
-                    console.log(this.state);
-                }
-                );
-                break;
-            }
+        this.setState({
+            history: history.concat([{squares: squares}]),
+            stepNumber: history.length,
+            xIsNext: !this.state.xIsNext
+        });
+    }
+    
+    componentDidUpdate(prevProps, prevState) {
+        console.log('componentDidUpdate');
+        const isComputerTurn = this.state.xIsNext ? false : true;
+        if (isComputerTurn) {
+            this.computerThinkAndPlay();
         }
     }
 
@@ -91,45 +270,37 @@ class App extends React.Component {
         const current = history[history.length - 1];
         const squares = current.squares.slice();
         
-        if (calculateWinner(squares) || squares[i]) {
-            return;
-        }
-
-
         const isComputerTurn = this.state.xIsNext ? false : true;
         
-        if (!isComputerTurn) {
-            console.log(`game click event ${i}`);
-            squares[i] = this.state.xIsNext ? "X" : "O";
-            console.log(squares);
-            console.log(history.concat([{squares: squares}]));
-            this.setState({
-                history: history.concat([
-                    {
-                        squares: squares
-                    }
-                ]),
-                stepNumber: history.length,
-                xIsNext: !this.state.xIsNext
-            }, () => {
-                console.log('call back');
-                console.log(this.state);
-                this.computerThinkAndPlay()
-            });
-        } 
-
+        let winner = calculateWinner(squares);
+        if (!winner) {
+            if (!isComputerTurn) {
+                if (!squares[i]) {
+                    squares[i] = this.state.xIsNext ? "X" : "O";
+                    this.setState({
+                        history: history.concat([
+                            {
+                                squares: squares
+                            }
+                        ]),
+                        stepNumber: history.length,
+                        xIsNext: !this.state.xIsNext
+                    });
+                }
+            } 
+        }
     }
 
     render() {
         const history = this.state.history;
-        const current = history[this.state.stepNumber];
+        const current = history[history.length - 1];
         let status;
         let winner = calculateWinner(current.squares);
+
         if (winner) {
             status = "Winner: " + winner;
         } else {
             status = "Next player: " + (this.state.xIsNext ? "X" : "O");
-            console.log(status);
         }
 
         return (
