26b5dc9cfa094efdbbc1fab0b430f716190fcf2d
tamlt2704 <tamlt2704@gmail.com>
2020-07-29 11:54:21 +0800

    02. REPL with statement prepare

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ var readline = require('readline')
 const CMD_PROMPT = 'db >'
 const STATUS = {
     'META_COMMAND_SUCCESS': 1,
-    'META_COMMAND_UNRECOGNIZED_COMMAND': -1
+    'META_COMMAND_UNRECOGNIZED': -1,
+    'PREPARE_SUCCESS': 2,
+    'PREPARE_STATEMENT_UNRECOGNIZED': -2,
+    'STATEMENT_INSERT': 3,
+    'STATEMENT_SELECT': 4
 }
 
 var rl = readline.createInterface({
@@ -15,22 +19,61 @@ function exit_db() {
     process.exit(0)
 }
 
+class Statement {
+    constructor() {
+        this.statementType = null;
+    }
+}
+
 function do_meta_command(command) {
     if (command === '.exit') {
         exit_db()
     } else {
-        return STATUS.META_COMMAND_UNRECOGNIZED_COMMAND
+        return STATUS.META_COMMAND_UNRECOGNIZED
+    }
+}
+
+function prepare_statement(command, statement) {
+    if (command.startsWith('insert')) {
+        statement.statementType = STATUS.STATEMENT_INSERT
+        return STATUS.PREPARE_SUCCESS
+    } else if (command.startsWith('select')) {
+        statement.statementType = STATUS.STATEMENT_SELECT
+        return STATUS.PREPARE_SUCCESS
+    }
+    return STATUS.PREPARE_STATEMENT_UNRECOGNIZED
+}
+
+function execute_statement(statement) {
+    switch (statement.statementType) {
+        case STATUS.STATEMENT_INSERT:
+            console.log( 'this is where we would do an insert' )
+            break
+        case STATUS.STATEMENT_SELECT:
+            console.log( 'this is where we would do an select' )
+            break
     }
 }
 
 rl.on('line', command => {
-    command = command.trim()
+    command = command.trim().toLowerCase()
 
     if (command.startsWith('.')) {
         switch (do_meta_command(command)) {
-            case STATUS.META_COMMAND_UNRECOGNIZED_COMMAND:
+            case STATUS.META_COMMAND_UNRECOGNIZED:
                 console.log( 'unrecognized command' )
                 break;
+        }
+    } else {
+        let statement = new Statement();
+        switch(prepare_statement(command, statement)) {
+            case STATUS.PREPARE_SUCCESS:
+                execute_statement(statement)
+                console.log( 'Executed!' )
+                break
+            case STATUS.PREPARE_STATEMENT_UNRECOGNIZED:
+                console.log( 'unrecognized statement' )
+                break
         }
     }
 
