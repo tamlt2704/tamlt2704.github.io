17f8d042bb9eeeeb04334476a1a73e0a2073cdf2
tamlt2704 <tamlt2704@gmail.com>
2024-11-22 11:25:24 +0800

    add github workflow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
 export default function Home() {
   return (
-    <div> Hello World!</div>
+    <div> Hello World</div>
   );
 }
