5d83c4ba39e23a1a2f3480577e20285c543aa591
tamlt2704 <tamlt2704@gmail.com>
2024-11-22 11:10:08 +0800

    move to nextjs

---
+++ next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: "export", // enable static exports -> next build will generate 'out' folder
+    reactStrictMode: true,
+};
+
+export default nextConfig;
