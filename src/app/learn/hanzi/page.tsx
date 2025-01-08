4b061f307c7a3a8b5dce39f011530bbe0c9ac030
tamlt2704 <tamlt2704@gmail.com>
2025-01-08 10:37:46 +0800

    hanzi flex-wrap

--- src/app/learn/hanzi/page.tsx
+++ src/app/learn/hanzi/page.tsx
@@ -118,7 +118,7 @@ export default function HanZiWriter() {
 
             <div ref={hanziRef} />
             <div
-                className="flex flex-row" 
+                className="flex flex-row flex-wrap" 
                 ref={fanoutRef} 
             />
             <button 
