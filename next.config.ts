48b0d2b62427e911ec8ff7130b0b5b8f13538929
tamlt2704 <tamlt2704@gmail.com>
2025-01-13 06:56:46 +0800

    init blog;

---
+++ next.config.ts
@@ -0,0 +1,14 @@
+import type { NextConfig } from "next";
+import createMDX from '@next/mdx'
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+};
+
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+  extension: /\.mdx?$/,
+})
+
+export default withMDX(nextConfig);
