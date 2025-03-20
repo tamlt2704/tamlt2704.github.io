aa911dfbdee5f31727c811468272cd50bee2e7c7
tamlt2704 <tamlt2704@gmail.com>
2025-03-20 22:51:36 +0800

    init page

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+
+
+const isProd = process.env.NODE_ENV === 'production';
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/tamlt2704.github.io/' : '',
+  basePath: isProd ? '/tamlt2704.github.io' : '',
+  output: 'export'
+};
 
 module.exports = nextConfig;
+// export default nextConfig;
