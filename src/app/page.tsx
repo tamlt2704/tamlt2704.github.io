dc7c47d0b4a6e015585b00df988970a3b6714f42
tamlt <tamlt2704@gmail.com>
2025-04-17 17:21:08 +0800

    Updates

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <div>
-      <Image src={'/matrix.jpg'} width={400} height={400} alt="matrix" />
-    </div>
-  );
-}
